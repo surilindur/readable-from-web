ca41ef92d4456b9f7a59512af40555f73d053c89
surilindur <surilindur@users.noreply.github.com>
2024-03-23 18:05:07 +0200

    Initial attempt to add browser tests

---
+++ karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    files: [
+      'index.js',
+      'lib/*.js',
+      'test/*.js',
+    ],
+    frameworks: [ 'mocha', 'chai' ],
+    reporters: [ 'progress', 'coverage' ],
+    preprocessors: {
+      'lib/*.js': [ 'coverage' ],
+    },
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/',
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: [ 'FirefoxHeadless', 'ChromiumHeadless', 'Edge' ],
+    singleRun: true,
+    concurrency: Number.POSITIVE_INFINITY,
+  });
+};
