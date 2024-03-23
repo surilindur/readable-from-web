451d255920ec879eae83e77a4fbbbe6782434c04
surilindur <surilindur@users.noreply.github.com>
2024-03-23 23:50:19 +0200

    Add TypeScript plugin for browser runner

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,6 +1,8 @@
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+
 module.exports = {
   files: 'test/*-test.ts',
   concurrency: 1,
   nodeResolve: true,
-  watch: true,
+  plugins: [ esbuildPlugin({ ts: true, target: 'es6' }) ],
 };
